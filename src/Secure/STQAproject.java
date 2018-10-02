e2ca9fa926106d89ce44a3ff5161f64bdb9a5814
VaishnaviK23 <vaishnavik.impressions@gmail.com>
2018-10-02 12:25:04 +0530

    initial netbeans files

---
+++ src/Secure/STQAproject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Secure;
+
+/**
+ *
+ * @author vuk ksc
+ */
+public class STQAproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginPage().setVisible(true);
+    }
+
+}
